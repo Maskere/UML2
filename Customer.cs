8949e01699b6d7a746f86fc5da247ec7ec6bc82f
Maskere <mstachkruse@gmail.com>
2024-04-04 00:07:50 +0200

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -12,14 +12,16 @@ namespace PizzaStore
         private string _customerName;
         private string _email;
         private string _phoneNumber;
+        private int _customerId;
         #endregion
 
         #region Constructor
-        public Customer(string Name)
+        public Customer(string Name, int CustomerId)
         {
             _customerName = Name;
             _phoneNumber = PhoneNumber;
             _email = Email;
+            _customerId = CustomerId;
         }
         #endregion
 
@@ -27,14 +29,22 @@ namespace PizzaStore
         public string CustomerName
         {
             get { return _customerName; }
+            set { _customerName = value; }
         }
         public string Email
         {
             get { return _email; }
+            set { _email = value; }
         }
         public string PhoneNumber
         {
             get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+        public int CustomerId 
+        {
+            get { return _customerId; }
+            set { _customerId = value; }
         }
         #endregion
 
